7f4c9766c47d98ce984f72e77afabe9547f28f80
mcolombo <mr.michele.colombo@gmail.com>
2019-01-25 18:23:33 +0100

    wip

--- examples/simple_expression/exp_definition.py
+++ examples/simple_expression/exp_definition.py
@@ -94,23 +94,18 @@ class BinaryExpressionTreeGen:
             return self.evaluate(et.children[0]) + self.evaluate(et.children[1])
 
 
-class LabelledBinaryExpressionTreeGen:
+class LabelledBinaryExpressionTreeGen(BinaryExpressionTreeGen):
 
     def __init__(self, min_value, max_value):
-
-        if min_value < 0 or min_value >= max_value:
-            raise ValueError()
-
-        self.NumValue = create_num_value(min_value, max_value)
+        super(LabelledBinaryExpressionTreeGen, self).__init__(min_value, max_value)
 
         self.tree_def = TreeDefinition(
             node_types=[
-                NodeDefinition("op_bin", may_root=True, arity=NodeDefinition.FixedArity(2), value_type=self.OpValue),
+                NodeDefinition("op_bin", may_root=True, arity=NodeDefinition.FixedArity(2), value_type=OpValue),
                 NodeDefinition("num_value", may_root=True, arity=NodeDefinition.FixedArity(0), value_type=self.NumValue)
 
             ])
 
-        self.leaf_values = list(range(min_value, max_value+1))
         self.node_types = self.tree_def.node_types
 
     def generate(self, max_depth):
@@ -169,7 +164,6 @@ class NaryExpressionTreeGen(BinaryExpressionTreeGen):
                                value_type=self.NumValue)
             ]
         )
-        self.leaf_values = list(range(min_value, max_value + 1))
         self.node_types = self.tree_def.node_types
 
     def generate(self, max_depth):
@@ -205,6 +199,62 @@ class NaryExpressionTreeGen(BinaryExpressionTreeGen):
             if t.node_type_id == 'add_n':
                 return sum(map(self.evaluate, t.children))
             if t.node_type_id == 'sub_bin':
+                return self.evaluate(t.children[0]) - self.evaluate(t.children[1])
+        else:
+            return t.value.abstract_value
+
+
+class LabelledNaryExpressionTreeGen(NaryExpressionTreeGen):
+    def __init__(self, min_value, max_value, max_arity):
+        super(LabelledNaryExpressionTreeGen, self).__init__(min_value, max_value, max_arity)
+
+        self.tree_def = TreeDefinition(
+            node_types=[
+                NodeDefinition("op_n", may_root=True,
+                               arity=NodeDefinition.VariableArity(min_value=2, max_value=max_arity),
+                               value_type=OpValue),
+                NodeDefinition("num_value", may_root=True, arity=NodeDefinition.FixedArity(0),
+                               value_type=self.NumValue)
+            ]
+        )
+        self.node_types = self.tree_def.node_types
+
+    def generate(self, max_depth):
+        """Generate a random arithmetic expression tree, using just n-ary plus and binary minus
+            Args:
+                max_depth: integer > 0
+            Returns:
+                expression tree where leaves are int.
+        """
+
+        if max_depth == 1: # recursion base case
+            v = random.sample(self.leaf_values, 1)[0]
+            return Tree(node_type_id='num_value', value=self.NumValue(abstract_value=v))
+
+        elif max_depth > 1:
+            types = self.node_types
+            node_type = random.sample(types, 1)[0]
+            o = random.sample(['+', '-'], 1)[0]
+
+            if node_type.id == 'num_value':
+                return self.generate(1)
+
+            elif node_type.id == 'op_n' and o == "+":
+                n = random.randint(node_type.arity.min_value, node_type.arity.max_value)
+                return Tree(node_type.id, children=[self.generate(max_depth - 1) for _ in range(n)],
+                            value=OpValue(abstract_value='+'))
+
+            elif node_type.id == 'op_n' and o == "-":
+                return Tree(node_type.id, children=[
+                    self.generate(max_depth - 1),
+                    self.generate(max_depth - 1)],
+                            value=OpValue(abstract_value='-'))
+
+    def evaluate(self, t: Tree):
+        if len(t.children) > 0:
+            if t.node_type_id == 'op_n' and t.value.abstract_value == "+":
+                return sum(map(self.evaluate, t.children))
+            if t.node_type_id == 'sub_bin' and t.value.abstract_value == '-':
                 return self.evaluate(t.children[0]) - self.evaluate(t.children[1])
         else:
             return t.value.abstract_value
